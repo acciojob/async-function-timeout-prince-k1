5d43893310e3a9c7843a1234c0a383f855940f20
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2024-05-10 16:01:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+document.getElementById('btn').addEventListener('click', async () => {
+	let text = document.getElementById('text').value;
+    let delay = document.getElementById('delay').value;
+    
+        let myPromise = new Promise ((res) => {
+            setTimeout(() => {
+                res(text);
+            } , delay)
+            
+        });
+        let resolve = await myPromise;
+        document.getElementById('output').innerText = resolve;
+    
+    
+});
